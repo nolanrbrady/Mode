ecb877730c6751241f519f1c4e6498547a0620a8
Nolan Brady <nolanrbrady@gmail.com>
2018-09-04 11:09:08 -0600

    initial commit

---
+++ mode/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  mode
+//
+//  Created by Nolan Brady on 9/1/18.
+//  Copyright © 2018 Nolan Brady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // Email
+    @IBOutlet weak var email: UITextField!
+    
+    // Password
+    @IBOutlet weak var password: UITextField!
+
+    // Handle Create an Account
+    @IBAction func _createAccount(_ sender: Any) {
+    }
+    // Handle Forgot password
+    @IBAction func _forgotPassword(_ sender: Any) {
+    }
+}
+
